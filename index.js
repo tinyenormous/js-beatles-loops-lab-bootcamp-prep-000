af222e8489e7e26e4b132190040a7ed16505e145
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 15:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ function theBeatlesPlay(arrayMusic,arrayInstrument){
   return newArray
 }
 // 2) Beatles Loops johnLennonFacts returns an array of strings with exclamationpoints:
-
+function johnLennonFacts(facts){
+  newArray=[]
+  var i =0
+  while (i<facts.length){
+    newArray.push()
+    i++
+}
 // 3) Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 :
 
 // 4) Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' once when passed the parameter 17:
