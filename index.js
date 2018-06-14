95c1aecaa52ed2756b721eadbbaf6992b89cdce6
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 15:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function johnLennonFacts(facts){
   var newArray=[]
   var i=0
   var exclMark = "!!!"
-  while (i < facts.length){
+  while (i < 2) {
     // newArray.push(facts[i].concat(exclMark))
     // i++
     i=i+1
