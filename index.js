670c9be68f616b4409c210f45e38828dce3e00e0
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 15:04:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 // 1) Beatles Loops returns an array of strings containing what instruments eachmusician plays:
-
+function theBeatlesPlay(arrayMusic,arrayInstrument){
+  var newArray=();
+  for (i=0,i<arrayMusic.length, i++) {
+    newArray.push()
+  }
+}
 // 2) Beatles Loops johnLennonFacts returns an array of strings with exclamationpoints:
 
 // 3) Beatles Loops iLoveTheBeatles returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 :
