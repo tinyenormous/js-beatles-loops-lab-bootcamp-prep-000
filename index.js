dcb997c01e8d7d7121841e0b360b3d54cf3c15ee
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 15:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function theBeatlesPlay(arrayMusic,arrayInstrument){
 function johnLennonFacts(facts){
   var newArray=[]
   var exclMark = '!!!'
-  var i = facts.length-1
+  var i = facts.length
   
   while (i > 0) {
     console.log(i)  
